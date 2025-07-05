ff9894648d733fed8568eaf825d43d1d03377043
shreeshant01 <avinashupadhaya99@gmail.com>
2025-07-05 20:10:05 +0545

    FIX: new hello world added

--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,7 @@
-public class helloWorld {
+public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Print Hello World twice
+        System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
